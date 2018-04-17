b753014ec8f8c37b5869962e516d7ba83e037793
hdezier <hdezier@mailjet.com>
2018-04-17 18:19:48 +0200

    wip

--- storage/tile38/actor.go
+++ storage/tile38/actor.go
@@ -5,22 +5,34 @@ import (
 	"github.com/go-redis/redis"
 )
 
+// CreateActor is the tile38 implementation for ActorService.
 func (s *Service) CreateActor(actors []game.Actor) error {
+	query := `
+
+	`
+	return s.Process(redis.NewStringCmd("SET", "actor", "%s", "point", %f, %f))
+}
+
+// UpdateActor is the tile38 implementation for ActorService.
+func (s *Service) UpdateActor(subset game.ActorSubset, patch game.ActorPatch) error {
 	query := `
 		SET actor %s point %f %f
 	`
-	cmd, err := redis.NewCmd()
-	return nil
+	return s.Process(redis.NewCmd(query))
 }
 
-func (s *Service) UpdateActor(subset game.ActorSubset, patch game.ActorPatch) error {
-	return nil
-}
-
+// DeleteActor is the tile38 implementation for ActorService.
 func (s *Service) DeleteActor(subset game.ActorSubset) error {
-	return nil
+	query := `
+		DEL actor %s
+	`
+	return s.Process(redis.NewCmd(query))
 }
 
+// ListActor is the tile38 implementation for ActorService.
 func (s *Service) ListActor(subset game.ActorSubset) ([]game.Actor, error) {
-	return nil, nil
+	query := `
+		SET actor %s point %f %f
+	`
+	return s.Process(redis.NewCmd(query))
 }
