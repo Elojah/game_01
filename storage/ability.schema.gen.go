6364b686e4ab13cf517857043e7295df9d657835
hdezier <hdezier@mailjet.com>
2018-05-22 17:34:25 +0200

    add ability comp schema

--- storage/ability.schema.gen.go
+++ storage/ability.schema.gen.go
@@ -12,6 +12,374 @@ var (
 	_ = time.Now()
 )
 
+type HealDirect struct {
+	Amount float64
+	Type   uint8
+}
+
+func (d *HealDirect) Size() (s uint64) {
+
+	s += 9
+	return
+}
+func (d *HealDirect) Marshal(buf []byte) ([]byte, error) {
+	size := d.Size()
+	{
+		if uint64(cap(buf)) >= size {
+			buf = buf[:size]
+		} else {
+			buf = make([]byte, size)
+		}
+	}
+	i := uint64(0)
+
+	{
+
+		v := *(*uint64)(unsafe.Pointer(&(d.Amount)))
+
+		buf[0+0] = byte(v >> 0)
+
+		buf[1+0] = byte(v >> 8)
+
+		buf[2+0] = byte(v >> 16)
+
+		buf[3+0] = byte(v >> 24)
+
+		buf[4+0] = byte(v >> 32)
+
+		buf[5+0] = byte(v >> 40)
+
+		buf[6+0] = byte(v >> 48)
+
+		buf[7+0] = byte(v >> 56)
+
+	}
+	{
+
+		buf[0+8] = byte(d.Type >> 0)
+
+	}
+	return buf[:i+9], nil
+}
+
+func (d *HealDirect) Unmarshal(buf []byte) (uint64, error) {
+	i := uint64(0)
+
+	{
+
+		v := 0 | (uint64(buf[0+0]) << 0) | (uint64(buf[1+0]) << 8) | (uint64(buf[2+0]) << 16) | (uint64(buf[3+0]) << 24) | (uint64(buf[4+0]) << 32) | (uint64(buf[5+0]) << 40) | (uint64(buf[6+0]) << 48) | (uint64(buf[7+0]) << 56)
+		d.Amount = *(*float64)(unsafe.Pointer(&v))
+
+	}
+	{
+
+		d.Type = 0 | (uint8(buf[0+8]) << 0)
+
+	}
+	return i + 9, nil
+}
+
+type DamageDirect struct {
+	Amount float64
+	Type   uint8
+}
+
+func (d *DamageDirect) Size() (s uint64) {
+
+	s += 9
+	return
+}
+func (d *DamageDirect) Marshal(buf []byte) ([]byte, error) {
+	size := d.Size()
+	{
+		if uint64(cap(buf)) >= size {
+			buf = buf[:size]
+		} else {
+			buf = make([]byte, size)
+		}
+	}
+	i := uint64(0)
+
+	{
+
+		v := *(*uint64)(unsafe.Pointer(&(d.Amount)))
+
+		buf[0+0] = byte(v >> 0)
+
+		buf[1+0] = byte(v >> 8)
+
+		buf[2+0] = byte(v >> 16)
+
+		buf[3+0] = byte(v >> 24)
+
+		buf[4+0] = byte(v >> 32)
+
+		buf[5+0] = byte(v >> 40)
+
+		buf[6+0] = byte(v >> 48)
+
+		buf[7+0] = byte(v >> 56)
+
+	}
+	{
+
+		buf[0+8] = byte(d.Type >> 0)
+
+	}
+	return buf[:i+9], nil
+}
+
+func (d *DamageDirect) Unmarshal(buf []byte) (uint64, error) {
+	i := uint64(0)
+
+	{
+
+		v := 0 | (uint64(buf[0+0]) << 0) | (uint64(buf[1+0]) << 8) | (uint64(buf[2+0]) << 16) | (uint64(buf[3+0]) << 24) | (uint64(buf[4+0]) << 32) | (uint64(buf[5+0]) << 40) | (uint64(buf[6+0]) << 48) | (uint64(buf[7+0]) << 56)
+		d.Amount = *(*float64)(unsafe.Pointer(&v))
+
+	}
+	{
+
+		d.Type = 0 | (uint8(buf[0+8]) << 0)
+
+	}
+	return i + 9, nil
+}
+
+type HealOverTime struct {
+	Amount    float64
+	Type      uint8
+	Frequency uint64
+	Duration  uint64
+}
+
+func (d *HealOverTime) Size() (s uint64) {
+
+	s += 25
+	return
+}
+func (d *HealOverTime) Marshal(buf []byte) ([]byte, error) {
+	size := d.Size()
+	{
+		if uint64(cap(buf)) >= size {
+			buf = buf[:size]
+		} else {
+			buf = make([]byte, size)
+		}
+	}
+	i := uint64(0)
+
+	{
+
+		v := *(*uint64)(unsafe.Pointer(&(d.Amount)))
+
+		buf[0+0] = byte(v >> 0)
+
+		buf[1+0] = byte(v >> 8)
+
+		buf[2+0] = byte(v >> 16)
+
+		buf[3+0] = byte(v >> 24)
+
+		buf[4+0] = byte(v >> 32)
+
+		buf[5+0] = byte(v >> 40)
+
+		buf[6+0] = byte(v >> 48)
+
+		buf[7+0] = byte(v >> 56)
+
+	}
+	{
+
+		buf[0+8] = byte(d.Type >> 0)
+
+	}
+	{
+
+		buf[0+9] = byte(d.Frequency >> 0)
+
+		buf[1+9] = byte(d.Frequency >> 8)
+
+		buf[2+9] = byte(d.Frequency >> 16)
+
+		buf[3+9] = byte(d.Frequency >> 24)
+
+		buf[4+9] = byte(d.Frequency >> 32)
+
+		buf[5+9] = byte(d.Frequency >> 40)
+
+		buf[6+9] = byte(d.Frequency >> 48)
+
+		buf[7+9] = byte(d.Frequency >> 56)
+
+	}
+	{
+
+		buf[0+17] = byte(d.Duration >> 0)
+
+		buf[1+17] = byte(d.Duration >> 8)
+
+		buf[2+17] = byte(d.Duration >> 16)
+
+		buf[3+17] = byte(d.Duration >> 24)
+
+		buf[4+17] = byte(d.Duration >> 32)
+
+		buf[5+17] = byte(d.Duration >> 40)
+
+		buf[6+17] = byte(d.Duration >> 48)
+
+		buf[7+17] = byte(d.Duration >> 56)
+
+	}
+	return buf[:i+25], nil
+}
+
+func (d *HealOverTime) Unmarshal(buf []byte) (uint64, error) {
+	i := uint64(0)
+
+	{
+
+		v := 0 | (uint64(buf[0+0]) << 0) | (uint64(buf[1+0]) << 8) | (uint64(buf[2+0]) << 16) | (uint64(buf[3+0]) << 24) | (uint64(buf[4+0]) << 32) | (uint64(buf[5+0]) << 40) | (uint64(buf[6+0]) << 48) | (uint64(buf[7+0]) << 56)
+		d.Amount = *(*float64)(unsafe.Pointer(&v))
+
+	}
+	{
+
+		d.Type = 0 | (uint8(buf[0+8]) << 0)
+
+	}
+	{
+
+		d.Frequency = 0 | (uint64(buf[0+9]) << 0) | (uint64(buf[1+9]) << 8) | (uint64(buf[2+9]) << 16) | (uint64(buf[3+9]) << 24) | (uint64(buf[4+9]) << 32) | (uint64(buf[5+9]) << 40) | (uint64(buf[6+9]) << 48) | (uint64(buf[7+9]) << 56)
+
+	}
+	{
+
+		d.Duration = 0 | (uint64(buf[0+17]) << 0) | (uint64(buf[1+17]) << 8) | (uint64(buf[2+17]) << 16) | (uint64(buf[3+17]) << 24) | (uint64(buf[4+17]) << 32) | (uint64(buf[5+17]) << 40) | (uint64(buf[6+17]) << 48) | (uint64(buf[7+17]) << 56)
+
+	}
+	return i + 25, nil
+}
+
+type DamageOverTime struct {
+	Amount    float64
+	Type      uint8
+	Frequency uint64
+	Duration  uint64
+}
+
+func (d *DamageOverTime) Size() (s uint64) {
+
+	s += 25
+	return
+}
+func (d *DamageOverTime) Marshal(buf []byte) ([]byte, error) {
+	size := d.Size()
+	{
+		if uint64(cap(buf)) >= size {
+			buf = buf[:size]
+		} else {
+			buf = make([]byte, size)
+		}
+	}
+	i := uint64(0)
+
+	{
+
+		v := *(*uint64)(unsafe.Pointer(&(d.Amount)))
+
+		buf[0+0] = byte(v >> 0)
+
+		buf[1+0] = byte(v >> 8)
+
+		buf[2+0] = byte(v >> 16)
+
+		buf[3+0] = byte(v >> 24)
+
+		buf[4+0] = byte(v >> 32)
+
+		buf[5+0] = byte(v >> 40)
+
+		buf[6+0] = byte(v >> 48)
+
+		buf[7+0] = byte(v >> 56)
+
+	}
+	{
+
+		buf[0+8] = byte(d.Type >> 0)
+
+	}
+	{
+
+		buf[0+9] = byte(d.Frequency >> 0)
+
+		buf[1+9] = byte(d.Frequency >> 8)
+
+		buf[2+9] = byte(d.Frequency >> 16)
+
+		buf[3+9] = byte(d.Frequency >> 24)
+
+		buf[4+9] = byte(d.Frequency >> 32)
+
+		buf[5+9] = byte(d.Frequency >> 40)
+
+		buf[6+9] = byte(d.Frequency >> 48)
+
+		buf[7+9] = byte(d.Frequency >> 56)
+
+	}
+	{
+
+		buf[0+17] = byte(d.Duration >> 0)
+
+		buf[1+17] = byte(d.Duration >> 8)
+
+		buf[2+17] = byte(d.Duration >> 16)
+
+		buf[3+17] = byte(d.Duration >> 24)
+
+		buf[4+17] = byte(d.Duration >> 32)
+
+		buf[5+17] = byte(d.Duration >> 40)
+
+		buf[6+17] = byte(d.Duration >> 48)
+
+		buf[7+17] = byte(d.Duration >> 56)
+
+	}
+	return buf[:i+25], nil
+}
+
+func (d *DamageOverTime) Unmarshal(buf []byte) (uint64, error) {
+	i := uint64(0)
+
+	{
+
+		v := 0 | (uint64(buf[0+0]) << 0) | (uint64(buf[1+0]) << 8) | (uint64(buf[2+0]) << 16) | (uint64(buf[3+0]) << 24) | (uint64(buf[4+0]) << 32) | (uint64(buf[5+0]) << 40) | (uint64(buf[6+0]) << 48) | (uint64(buf[7+0]) << 56)
+		d.Amount = *(*float64)(unsafe.Pointer(&v))
+
+	}
+	{
+
+		d.Type = 0 | (uint8(buf[0+8]) << 0)
+
+	}
+	{
+
+		d.Frequency = 0 | (uint64(buf[0+9]) << 0) | (uint64(buf[1+9]) << 8) | (uint64(buf[2+9]) << 16) | (uint64(buf[3+9]) << 24) | (uint64(buf[4+9]) << 32) | (uint64(buf[5+9]) << 40) | (uint64(buf[6+9]) << 48) | (uint64(buf[7+9]) << 56)
+
+	}
+	{
+
+		d.Duration = 0 | (uint64(buf[0+17]) << 0) | (uint64(buf[1+17]) << 8) | (uint64(buf[2+17]) << 16) | (uint64(buf[3+17]) << 24) | (uint64(buf[4+17]) << 32) | (uint64(buf[5+17]) << 40) | (uint64(buf[6+17]) << 48) | (uint64(buf[7+17]) << 56)
+
+	}
+	return i + 25, nil
+}
+
 type Ability struct {
 	ID            [16]byte
 	Type          [16]byte
@@ -19,6 +387,7 @@ type Ability struct {
 	MPConsumption uint64
 	CD            uint32
 	CurrentCD     uint32
+	Components    []interface{}
 }
 
 func (d *Ability) Size() (s uint64) {
@@ -43,6 +412,82 @@ func (d *Ability) Size() (s uint64) {
 
 		}
 		s += l
+	}
+	{
+		l := uint64(len(d.Components))
+
+		{
+
+			t := l
+			for t >= 0x80 {
+				t >>= 7
+				s++
+			}
+			s++
+
+		}
+
+		for k0 := range d.Components {
+
+			{
+				var v uint64
+				switch d.Components[k0].(type) {
+
+				case HealDirect:
+					v = 0 + 1
+
+				case DamageDirect:
+					v = 1 + 1
+
+				case HealOverTime:
+					v = 2 + 1
+
+				case DamageOverTime:
+					v = 3 + 1
+
+				}
+
+				{
+
+					t := v
+					for t >= 0x80 {
+						t >>= 7
+						s++
+					}
+					s++
+
+				}
+				switch tt := d.Components[k0].(type) {
+
+				case HealDirect:
+
+					{
+						s += tt.Size()
+					}
+
+				case DamageDirect:
+
+					{
+						s += tt.Size()
+					}
+
+				case HealOverTime:
+
+					{
+						s += tt.Size()
+					}
+
+				case DamageOverTime:
+
+					{
+						s += tt.Size()
+					}
+
+				}
+			}
+
+		}
+
 	}
 	s += 16
 	return
@@ -126,6 +571,102 @@ func (d *Ability) Marshal(buf []byte) ([]byte, error) {
 		buf[i+3+12] = byte(d.CurrentCD >> 24)
 
 	}
+	{
+		l := uint64(len(d.Components))
+
+		{
+
+			t := uint64(l)
+
+			for t >= 0x80 {
+				buf[i+16] = byte(t) | 0x80
+				t >>= 7
+				i++
+			}
+			buf[i+16] = byte(t)
+			i++
+
+		}
+		for k0 := range d.Components {
+
+			{
+				var v uint64
+				switch d.Components[k0].(type) {
+
+				case HealDirect:
+					v = 0 + 1
+
+				case DamageDirect:
+					v = 1 + 1
+
+				case HealOverTime:
+					v = 2 + 1
+
+				case DamageOverTime:
+					v = 3 + 1
+
+				}
+
+				{
+
+					t := uint64(v)
+
+					for t >= 0x80 {
+						buf[i+16] = byte(t) | 0x80
+						t >>= 7
+						i++
+					}
+					buf[i+16] = byte(t)
+					i++
+
+				}
+				switch tt := d.Components[k0].(type) {
+
+				case HealDirect:
+
+					{
+						nbuf, err := tt.Marshal(buf[i+16:])
+						if err != nil {
+							return nil, err
+						}
+						i += uint64(len(nbuf))
+					}
+
+				case DamageDirect:
+
+					{
+						nbuf, err := tt.Marshal(buf[i+16:])
+						if err != nil {
+							return nil, err
+						}
+						i += uint64(len(nbuf))
+					}
+
+				case HealOverTime:
+
+					{
+						nbuf, err := tt.Marshal(buf[i+16:])
+						if err != nil {
+							return nil, err
+						}
+						i += uint64(len(nbuf))
+					}
+
+				case DamageOverTime:
+
+					{
+						nbuf, err := tt.Marshal(buf[i+16:])
+						if err != nil {
+							return nil, err
+						}
+						i += uint64(len(nbuf))
+					}
+
+				}
+			}
+
+		}
+	}
 	return buf[:i+16], nil
 }
 
@@ -174,6 +715,108 @@ func (d *Ability) Unmarshal(buf []byte) (uint64, error) {
 
 		d.CurrentCD = 0 | (uint32(buf[i+0+12]) << 0) | (uint32(buf[i+1+12]) << 8) | (uint32(buf[i+2+12]) << 16) | (uint32(buf[i+3+12]) << 24)
 
+	}
+	{
+		l := uint64(0)
+
+		{
+
+			bs := uint8(7)
+			t := uint64(buf[i+16] & 0x7F)
+			for buf[i+16]&0x80 == 0x80 {
+				i++
+				t |= uint64(buf[i+16]&0x7F) << bs
+				bs += 7
+			}
+			i++
+
+			l = t
+
+		}
+		if uint64(cap(d.Components)) >= l {
+			d.Components = d.Components[:l]
+		} else {
+			d.Components = make([]interface{}, l)
+		}
+		for k0 := range d.Components {
+
+			{
+				v := uint64(0)
+
+				{
+
+					bs := uint8(7)
+					t := uint64(buf[i+16] & 0x7F)
+					for buf[i+16]&0x80 == 0x80 {
+						i++
+						t |= uint64(buf[i+16]&0x7F) << bs
+						bs += 7
+					}
+					i++
+
+					v = t
+
+				}
+				switch v {
+
+				case 0 + 1:
+					var tt HealDirect
+
+					{
+						ni, err := tt.Unmarshal(buf[i+16:])
+						if err != nil {
+							return 0, err
+						}
+						i += ni
+					}
+
+					d.Components[k0] = tt
+
+				case 1 + 1:
+					var tt DamageDirect
+
+					{
+						ni, err := tt.Unmarshal(buf[i+16:])
+						if err != nil {
+							return 0, err
+						}
+						i += ni
+					}
+
+					d.Components[k0] = tt
+
+				case 2 + 1:
+					var tt HealOverTime
+
+					{
+						ni, err := tt.Unmarshal(buf[i+16:])
+						if err != nil {
+							return 0, err
+						}
+						i += ni
+					}
+
+					d.Components[k0] = tt
+
+				case 3 + 1:
+					var tt DamageOverTime
+
+					{
+						ni, err := tt.Unmarshal(buf[i+16:])
+						if err != nil {
+							return 0, err
+						}
+						i += ni
+					}
+
+					d.Components[k0] = tt
+
+				default:
+					d.Components[k0] = nil
+				}
+			}
+
+		}
 	}
 	return i + 16, nil
 }
