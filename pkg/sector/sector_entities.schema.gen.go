91718716db180176b8a0d15dedd0fc863f2847c7
elojah <swingcastor@gmail.com>
2018-07-20 22:29:53 +0200

    move sectorentity marshal into domain

--- pkg/sector/sector_entities.schema.gen.go
+++ pkg/sector/sector_entities.schema.gen.go
@@ -1,4 +1,4 @@
-package storage
+package sector
 
 import (
 	"io"
@@ -12,42 +12,29 @@ var (
 	_ = time.Now()
 )
 
-type SectorEntities struct {
-	SectorID  [16]byte
-	EntityIDs [][16]byte
-}
-
-func (d *SectorEntities) Size() (s uint64) {
-
+func (d *Entities) Size() (s uint64) {
 	{
 		s += 16
 	}
 	{
 		l := uint64(len(d.EntityIDs))
-
 		{
-
 			t := l
 			for t >= 0x80 {
 				t >>= 7
 				s++
 			}
 			s++
-
 		}
-
 		for _ = range d.EntityIDs {
-
 			{
 				s += 16
 			}
-
 		}
-
 	}
 	return
 }
-func (d *SectorEntities) Marshal(buf []byte) ([]byte, error) {
+func (d *Entities) Marshal(buf []byte) ([]byte, error) {
 	size := d.Size()
 	{
 		if uint64(cap(buf)) >= size {
@@ -57,62 +44,49 @@ func (d *SectorEntities) Marshal(buf []byte) ([]byte, error) {
 		}
 	}
 	i := uint64(0)
-
 	{
-		copy(buf[i+0:], d.SectorID[:])
+		copy(buf[i:], d.SectorID[:])
 		i += 16
 	}
 	{
 		l := uint64(len(d.EntityIDs))
-
 		{
-
 			t := uint64(l)
-
 			for t >= 0x80 {
-				buf[i+0] = byte(t) | 0x80
+				buf[i] = byte(t) | 0x80
 				t >>= 7
 				i++
 			}
-			buf[i+0] = byte(t)
+			buf[i] = byte(t)
 			i++
-
 		}
 		for k0 := range d.EntityIDs {
-
 			{
-				copy(buf[i+0:], d.EntityIDs[k0][:])
+				copy(buf[i:], d.EntityIDs[k0][:])
 				i += 16
 			}
-
 		}
 	}
-	return buf[:i+0], nil
+	return buf[:i], nil
 }
-
-func (d *SectorEntities) Unmarshal(buf []byte) (uint64, error) {
+func (d *Entities) Unmarshal(buf []byte) (uint64, error) {
 	i := uint64(0)
-
 	{
-		copy(d.SectorID[:], buf[i+0:])
+		copy(d.SectorID[:], buf[i:])
 		i += 16
 	}
 	{
 		l := uint64(0)
-
 		{
-
 			bs := uint8(7)
-			t := uint64(buf[i+0] & 0x7F)
-			for buf[i+0]&0x80 == 0x80 {
+			t := uint64(buf[i] & 0x7F)
+			for buf[i]&0x80 == 0x80 {
 				i++
-				t |= uint64(buf[i+0]&0x7F) << bs
+				t |= uint64(buf[i]&0x7F) << bs
 				bs += 7
 			}
 			i++
-
 			l = t
-
 		}
 		if uint64(cap(d.EntityIDs)) >= l {
 			d.EntityIDs = d.EntityIDs[:l]
@@ -120,12 +94,10 @@ func (d *SectorEntities) Unmarshal(buf []byte) (uint64, error) {
 			d.EntityIDs = make([][16]byte, l)
 		}
 		for k0 := range d.EntityIDs {
-
 			{
-				copy(d.EntityIDs[k0][:], buf[i+0:])
+				copy(d.EntityIDs[k0][:], buf[i:])
 				i += 16
 			}
-
 		}
 	}
 	return i + 0, nil
