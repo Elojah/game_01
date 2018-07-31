d5d1ae55d7ae9c3843441077292a50adef45e40f
hdezier <hdezier@mailjet.com>
2018-07-31 10:50:35 +0200

    add sector check in integration

--- cmd/integration/static_data.go
+++ cmd/integration/static_data.go
@@ -26,6 +26,15 @@ type abilityTemplate struct {
 	Message          string
 }
 
+type sector struct {
+	Level   string
+	Exe     string
+	Method  string
+	Route   string
+	Sectors int
+	Message string
+}
+
 func expectEntityTemplates(a *LogAnalyzer) error {
 	file, err := os.Open("./static/entity_templates.json")
 	if err != nil {
@@ -92,6 +101,39 @@ func expectAbilityTemplates(a *LogAnalyzer) error {
 	})
 }
 
+func expectSector(a *LogAnalyzer) error {
+	file, err := os.Open("./static/sector.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	resp, err := http.Post("https://localhost:8081/sector", "application/json", file)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code %d", resp.StatusCode)
+	}
+	expected := sector{
+		Level:   "info",
+		Exe:     "./bin/game_tool",
+		Method:  "POST",
+		Route:   "/sector",
+		Sectors: 2,
+		Message: "found",
+	}
+	return a.Expect(func(s string) (bool, error) {
+		var et sector
+		if err := json.Unmarshal([]byte(s), &et); err != nil {
+			return false, err
+		}
+		if et != expected {
+			return false, fmt.Errorf("unexpected log %s", s)
+		}
+		return true, nil
+	})
+}
+
 func expectStaticData(a *LogAnalyzer) error {
 	// ignore certificate validity
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
@@ -99,6 +141,9 @@ func expectStaticData(a *LogAnalyzer) error {
 		return err
 	}
 	if err := expectAbilityTemplates(a); err != nil {
+		return err
+	}
+	if err := expectSector(a); err != nil {
 		return err
 	}
 	return nil
