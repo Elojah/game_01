72799abd84d6e7ec3a30afc93436dc2dbc187df2
hdezier <hdezier@mailjet.com>
2018-08-27 17:47:04 +0200

    wip

--- cmd/core/perform.go
+++ cmd/core/perform.go
@@ -22,12 +22,66 @@ func (a *app) Perform(id ulid.ID, e event.E) error {
 }
 
 func (a *app) PerformTarget(id ulid.ID, e event.E) error {
+
+	perform := e.Action.GetValue().(*event.Perform)
+
+	// #Retrieve source state.
+	source, err := a.EntityStore.GetEntity(entity.Subset{ID: perform.Source, MaxTS: e.TS.UnixNano()})
+	if err != nil {
+		return errors.Wrapf(err, "get entity %s at max ts %s", id.String(), e.TS.UnixNano())
+	}
+
+	// #Retrieve previous target state.
+	target, err := a.EntityStore.GetEntity(entity.Subset{ID: id, MaxTS: e.TS.UnixNano()})
+	if err != nil {
+		return errors.Wrapf(err, "get entity %s at max ts %s", id.String(), e.TS.UnixNano())
+	}
+
+	// #Retrieve ability.
+	ab, err := a.AbilityStore.GetAbility(ability.Subset{
+		ID: perform.AbilityID,
+	})
+	if err == gerrors.ErrNotFound {
+		return errors.Wrapf(gerrors.ErrInsufficientACLs, "get ability %s for %s", perform.AbilityID.String(), id.String())
+	}
+	if err != nil {
+		return errors.Wrapf(err, "get ability %s for %s", perform.AbilityID.String(), id.String())
+	}
+
+	// #Initialize feedback.
+	fb := ability.Feedback{
+		ID:        ulid.NewID(),
+		AbilityID: ab.ID,
+	}
+	// #Apply all ability components.
+	var result *multierror.Error
+	for i, comp := range ab.Components {
+		c := comp.GetValue()
+		switch c.(type) {
+		case ability.Damage:
+			cfb, err := target.Damage(source, c.(ability.Damage))
+			if err != nil {
+				result = multierror.Append(result, errors.Wrapf(err, "damage direct component %d", i))
+				continue
+			}
+			fb.Components = append(fb.Components, cfb)
+		case ability.Heal:
+			result = multierror.Append(result, gerrors.ErrNotImplementedYet)
+		case ability.HealOverTime:
+			result = multierror.Append(result, gerrors.ErrNotImplementedYet)
+		case ability.DamageOverTime:
+			result = multierror.Append(result, gerrors.ErrNotImplementedYet)
+		default:
+			result = multierror.Append(result, gerrors.ErrNotImplementedYet)
+		}
+	}
+
 	return nil
 }
 
 func (a *app) PerformSource(id ulid.ID, e event.E) error {
 
-	p := e.Action.GetValue().(*event.Perform)
+	perform := e.Action.GetValue().(*event.Perform)
 
 	// #Retrieve entity
 	source, err := a.EntityStore.GetEntity(entity.Subset{
@@ -40,28 +94,27 @@ func (a *app) PerformSource(id ulid.ID, e event.E) error {
 
 	// #Retrieve ability.
 	ab, err := a.AbilityStore.GetAbility(ability.Subset{
-		ID: p.AbilityID,
+		ID: perform.AbilityID,
 	})
 	if err == gerrors.ErrNotFound {
-		return errors.Wrapf(gerrors.ErrInsufficientACLs, "get ability %s for %s", p.AbilityID.String(), id.String())
+		return errors.Wrapf(gerrors.ErrInsufficientACLs, "get ability %s for %s", perform.AbilityID.String(), id.String())
 	}
 	if err != nil {
-		return errors.Wrapf(err, "get ability %s for %s", p.AbilityID.String(), id.String())
+		return errors.Wrapf(err, "get ability %s for %s", perform.AbilityID.String(), id.String())
 	}
 
 	// #Check cast was not interrupted.
 	if source.Cast == nil ||
-		source.Cast.AbilityID != p.AbilityID ||
+		source.Cast.AbilityID != perform.AbilityID ||
 		source.Cast.TS.Add(ab.CastTime) != e.TS {
 		// normal behavior, don't return errors
 		return nil
 	}
 
 	// #Send event to all targets
-	if len(p.Targets.Positions) != 0 {
+	if len(perform.Targets.Positions) != 0 {
 		return gerrors.ErrNotImplementedYet
 	}
-
 	var result *multierror.Error
 	errC := make(chan error, 0)
 	go func() {
@@ -70,15 +123,15 @@ func (a *app) PerformSource(id ulid.ID, e event.E) error {
 		}
 	}()
 	var wg sync.WaitGroup
-	wg.Add(len(p.Targets.Entities))
-	for _, ens := range p.Targets.Entities {
+	wg.Add(len(perform.Targets.Entities))
+	for _, ens := range perform.Targets.Entities {
 		for _, id := range ens.IDs {
 			go func(id ulid.ID) {
 				if err := a.EventQStore.PublishEvent(event.E{
 					ID: ulid.NewID(),
 					TS: e.TS.Add(time.Nanosecond), // Add TS + 1 ns to apply damage
 					Action: event.Action{
-						Perform: p,
+						Perform: perform,
 					},
 				}, id); err != nil {
 					errC <- err
