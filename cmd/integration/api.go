4b5c733978c96999779c2f79e2c3c222fb5bee04
elojah <swingcastor@gmail.com>
2018-09-03 12:10:21 +0200

    wip

--- cmd/integration/api.go
+++ cmd/integration/api.go
@@ -3,8 +3,24 @@ package main
 import (
 	"github.com/elojah/game_01/pkg/account"
 	"github.com/elojah/game_01/pkg/entity"
+	"github.com/elojah/mux/client"
 )
 
+/*
+#Test plan
+- SUCCESS Move same sector
+- FAIL Move same sector too far
+- SUCCESS Move neighbour sector
+- FAIL Move not neighbour sector
+- FAIL Move neighbour sector too far
+*/
+
 func expectAPI(a *LogAnalyzer, tok account.Token, ent entity.E) error {
+	var c client.C
+	c.Dial(client.Config{
+		Middlewares: []string{"lz4"},
+		PacketSize:  1024,
+	})
+
 	return nil
 }
