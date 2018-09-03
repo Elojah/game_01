e243b5c570979f830b4820628780f86b83286300
elojah <swingcastor@gmail.com>
2018-09-03 14:29:23 +0200

    wip

--- cmd/integration/api.go
+++ cmd/integration/api.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"net"
+	"time"
+
 	"github.com/elojah/game_01/pkg/account"
 	"github.com/elojah/game_01/pkg/entity"
+	"github.com/elojah/game_01/pkg/event"
+	"github.com/elojah/game_01/pkg/geometry"
+	"github.com/elojah/game_01/pkg/ulid"
 	"github.com/elojah/mux/client"
 )
 
@@ -21,6 +28,33 @@ func expectAPI(a *LogAnalyzer, tok account.Token, ent entity.E) error {
 		Middlewares: []string{"lz4"},
 		PacketSize:  1024,
 	})
+
+	// #SUCCESS Move same sector
+	moveSameSector := event.DTO{
+		ID:    ulid.NewID(),
+		Token: tok.ID,
+		TS:    time.Now(),
+		Query: event.Query{
+			Move: &event.Move{
+				Source:  ent.ID,
+				Targets: []ulid.ID{ent.ID},
+				Position: geometry.Position{
+					SectorID: ulid.MustParse("01CF001HTBA3CDR1ERJ6RF183A"),
+					// TODO ?
+					Coord: geometry.Vec3{},
+				},
+			},
+		},
+	}
+	raw, err := moveSameSector.Marshal()
+	if err != nil {
+		return fmt.Errorf("failed to marshal payload")
+	}
+	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3400")
+	if err != nil {
+		return err
+	}
+	c.Send(raw, addr)
 
 	return nil
 }
