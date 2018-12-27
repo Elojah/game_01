40b914bcea94aad826c00a8950ae490156a4c4b1
hdezier <swingcastor@gmail.com>
2018-12-27 17:36:51 +0100

    wip

--- pkg/event/svc/trigger.go
+++ pkg/event/svc/trigger.go
@@ -8,11 +8,13 @@ import (
 	gulid "github.com/elojah/game_01/pkg/ulid"
 )
 
+// TriggerService implements set event with trigger interactions
 type TriggerService struct {
 	event.TriggerStore
 	event.Store
 }
 
+// Set event if necessary considering trigger update or removal.
 func (s *TriggerService) Set(e event.E, entityID gulid.ID) error {
 	var t gulid.ID
 	err := gerrors.ErrNotFound
